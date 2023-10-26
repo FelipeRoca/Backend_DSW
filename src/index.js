6cceacc3d6bb381cd448fce409d3a3796dc53a01
FelipeRoca <feliroca01@gmail.com>
2023-10-26 19:47:17 -0300

    httpClient

--- src/index.js
+++ src/index.js
@@ -1,40 +1,78 @@
-import express from 'express'
-import {sequelize} from './database/connect.js'
-import usersRouter from './controllers/users.js' 
-import reviewsRouter from './controllers/reviews.js'
-import countriesRouter from './controllers/countries.js'
-import citiesRouter from './controllers/cities.js'
-import './models/User.js'
-import './models/Country.js'
+// import express from 'express'
+// import {sequelize} from './database/connect.js'
+// import usersRouter from './controllers/users.js' 
+// import reviewsRouter from './controllers/reviews.js'
+// import countriesRouter from './controllers/countries.js'
+// import citiesRouter from './controllers/cities.js'
+// import './models/User.js'
+// import './models/Country.js'
 
 
 
-const app = express()
+// const app = express()
 
-app.use(express.json())
+// app.use(express.json())
 
+
+// try {
+//     sequelize.authenticate()
+//     console.log('Db connected')
+// } catch (error) {
+//     console.error(error)
+// }
+
+
+
+// app.get('/', (req, res) => {
+//     res.json({message: 'todo ok'})
+// })
+
+
+// const PORT =   process.env.PORT || 3001
+// app.listen(PORT, () => { 
+//     console.log(`server on in port ${PORT}`)   //si no pones las comillas invertidas no reconoce a la variable PORT 
+// })
+
+// app.use(usersRouter)
+// app.use(reviewsRouter)
+// app.use(countriesRouter)
+// app.use(citiesRouter)
+
+
+import express from 'express';
+import cors from 'cors'; // Importa el módulo CORS
+import { sequelize } from './database/connect.js';
+import usersRouter from './controllers/users.js';
+import reviewsRouter from './controllers/reviews.js';
+import countriesRouter from './controllers/countries.js';
+import citiesRouter from './controllers/cities.js';
+import './models/User.js';
+import './models/Country.js';
+
+const app = express();
+
+app.use(express.json());
 
 try {
-    sequelize.authenticate()
-    console.log('Db connected')
+    sequelize.authenticate();
+    console.log('Db connected');
 } catch (error) {
-    console.error(error)
+    console.error(error);
 }
 
-
+// Configura CORS para permitir solicitudes desde cualquier origen
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.json({message: 'todo ok'})
-})
+    res.json({ message: 'todo ok' });
+});
 
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`server on in port ${PORT}`);
+});
 
-const PORT =   process.env.PORT || 3001
-app.listen(PORT, () => { 
-    console.log(`server on in port ${PORT}`)   //si no pones las comillas invertidas no reconoce a la variable PORT 
-})
-
-app.use(usersRouter)
-app.use(reviewsRouter)
-app.use(countriesRouter)
-app.use(citiesRouter)
-//quede en el video 3
+app.use(usersRouter);
+app.use(reviewsRouter);
+app.use(countriesRouter);
+app.use(citiesRouter);
